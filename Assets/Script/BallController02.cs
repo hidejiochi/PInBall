262569532d045f3cb6d95912d1b8cc9918c0d651
hidejiochi <platitude000@gmail.com>
2016-09-16 21:47:43 +0900

    Lesson5　得点加算課題　提出

---
+++ Assets/Script/BallController02.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController02 : MonoBehaviour {
+
+    private GameObject scoreText;
+
+    int score = 0; 
+    int[] points = { 10, 20, 30, 40 };
+           
+    
+	// Use this for initialization
+	void Start () {
+
+        this.scoreText = GameObject.Find("ScoreText");
+        this.scoreText.GetComponent<Text>().text = "Score: " + score;
+
+                           
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.scoreText.GetComponent<Text>().text = "Score: " + score;
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += points[0];
+
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += points[1];
+
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.score += points[2];
+
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += points[3];
+        }
+    }
+}
---
+++ Assets/Script/test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int[] points = { 10, 20 };
+
+        int a = 20;
+
+        int b = a + points[1];
+
+        Debug.Log(b);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
